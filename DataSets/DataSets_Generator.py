0f6ad3074f97899cbda447f8eea8e610acf05b43
szachovy <wjmaj98@gmail.com>
2019-05-05 17:13:25 +0200

    Solved ConfigParser problem, try to improve it.

--- DataSets/DataSets_Generator.py
+++ DataSets/DataSets_Generator.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
+from configparser
 
-from 
-
-class Binary_Representation(object):
-    def __init__(self, population, genes):
-        self.population = population
-        self.genes = genes
+class Population_Generator(object):
+    config = configparser.ConfigParser()
+    def __init__(self, population_size=config['GENERATOR']['POPULATION_SIZE'], chromosome_size=config['GENERATOR']['CHROMOSOME_SIZE'], equal_chromosomes=config['GENERATOR']['EQUAL_CHROMOSOMES'], initialization_method=config['GENERATOR']['INITIALIZATION_METHOD'], representation=config['GENERATOR']['REPRESENTATION'], saving_method=config['GENERATOR']['SAVING_METHOD']):
+        self.population_size = population_size
+        self.chromosome_size = chromosome_size
+        self.equal_chromosomes = equal_chromosomes
+        self.initialization_method = initialization_method
+        self.representation = representation
+        self.saving_method = saving_method
+        
 
     def binary_representation(self):
         pass
@@ -15,5 +20,5 @@ class Binary_Representation(object):
 
     def permutation_representation(self):
         pass
-        
-        
+
+print(Population_Generator().population_size)

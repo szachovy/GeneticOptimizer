585b0c38b0461447daece2afcb883c0166c3c1ed
szachovy <wjmaj98@gmail.com>
2019-05-05 13:20:36 +0200

    Initialized DataSets_Generator

---
+++ DataSets/DataSets_Generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from 
+
+class Binary_Representation(object):
+    def __init__(self, population, genes):
+        self.population = population
+        self.genes = genes
+
+    def binary_representation(self):
+        pass
+
+    def integer_representation(self):
+        pass
+
+    def permutation_representation(self):
+        pass
+        
+        
---
+++ tests/test_DataSets_Generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import pytest
+from DataSets import DataSets_Generator
+
